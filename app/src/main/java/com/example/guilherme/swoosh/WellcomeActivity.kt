118c875ffe0ef1f6d71b22bb417ab9b83f242185
Guilherme Welter <guilherme.a.welter@gmail.com>
2017-10-29 00:39:01 -0200

    Initial commit

---
+++ app/src/main/java/com/example/guilherme/swoosh/WellcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guilherme.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WellcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wellcome)
+    }
+}
