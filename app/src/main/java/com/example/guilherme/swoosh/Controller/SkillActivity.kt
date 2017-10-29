d4769af165526d6e3466e6a902a05d1f3259a134
Guilherme <guilherme.a.welter@gmail.com>
2017-10-29 13:22:36 -0200

    Adicionando mensagem final de acordo com a escolha do usuário

--- app/src/main/java/com/example/guilherme/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/guilherme/swoosh/Controller/SkillActivity.kt
@@ -1,17 +1,48 @@
 package com.example.guilherme.swoosh.Controller
 
+import android.content.Intent
 import android.os.Bundle
-import com.example.guilherme.swoosh.Utilities.EXTRA_LEAGUE
+import android.view.View
+import android.widget.Toast
 import com.example.guilherme.swoosh.R
+import com.example.guilherme.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.guilherme.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
     var league = ""
+    var skill = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
 
         league = intent.getStringExtra(EXTRA_LEAGUE) as String
+    }
+
+    fun onBegginerClick(view: View) {
+        begginer_skill_button.isChecked = true
+        baller_skill_button.isChecked = false
+
+        skill = "begginer"
+    }
+
+    fun onBallerClick(view: View) {
+        begginer_skill_button.isChecked = false
+        baller_skill_button.isChecked = true
+
+        skill = "baller"
+    }
+
+    fun onSkillFinishClick(view: View) {
+        if(skill != "") {
+            val finishActivity = Intent(this, FinishActivity::class.java)
+            finishActivity.putExtra(EXTRA_LEAGUE, league)
+            finishActivity.putExtra(EXTRA_SKILL, skill)
+            startActivity(finishActivity)
+        } else {
+            Toast.makeText(this, "Please select a skill level.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/guilherme/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/example/guilherme/swoosh/Utilities/Constants.kt
@@ -4,3 +4,4 @@ package com.example.guilherme.swoosh.Utilities
  * Created by Guilherme on 29/10/2017.
  */
 const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
